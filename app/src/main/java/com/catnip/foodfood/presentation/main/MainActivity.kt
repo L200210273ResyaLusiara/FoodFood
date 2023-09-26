42b3537abb32a5fba3bee523020e721f4f7d3324
LAPTOP-JK6TNGGP\asus <l200210273@student.ums.ac.id>
2023-09-26 15:53:56 +0700

    push asset

---
+++ app/src/main/java/com/catnip/foodfood/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.catnip.foodfood.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.catnip.foodfood.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
